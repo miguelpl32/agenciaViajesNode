d032c0aa06141ce11e8bb765d50703964ec39519
Miguel Pariente <66026985+miguelpl32@users.noreply.github.com>
2023-05-24 22:08:26 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./variables.env" });
 
 const db = new Sequelize(
-  mysql -hcontainers-us-west-71.railway.app -uroot -pLhilj41QcLP2FTsZeNh6 --port 7113 --protocol=TCP railway
+  'mysql -hcontainers-us-west-71.railway.app -uroot -pLhilj41QcLP2FTsZeNh6 --port 7113 --protocol=TCP railway'
  /* process.env.DATABASE,
   process.env.USERNAME,
   process.env.PASSWORD,
