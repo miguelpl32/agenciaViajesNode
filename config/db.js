af8acbe11c899b164cca00df2169dec26841d081
Miguel Pariente <66026985+miguelpl32@users.noreply.github.com>
2023-05-25 20:51:35 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,32 +2,26 @@ import Sequelize from "sequelize";
 import dotenv from "dotenv";
 dotenv.config({ path: "./variables.env" });
 
-var BD_NAME;
-var BD_USER;
-var BD_PASSWORD;
-
-
 const db = new Sequelize(
-  BD_NAME= 'railway',
-  BD_USER= 'root',
-  BD_PASSWORD='Lhilj41QcLP2FTsZeNh6',
+  process.env.BD_NAME,
+  process.env.BD_USER,
+  process.env.BD_PASSWORD,
   {
-    host:'containers-us-west-71.railway.app',
-    dialect: 'mysql',
-    port: 7113,
+    host: process.env.HOST,
+    port: process.env.PORT,
+    dialect: "mysql",
     define: {
       timestamps: false,
     },
     pool: {
       max: 5,
-      min: 1,
-      acquire: 80000,
-      idle: 50000,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
     },
     operatorAliases: false,
-  }
+
+ }
 );
 
 export default db;
-
-
